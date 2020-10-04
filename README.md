bc457b3fa0973b409984e37994aa6fd6d3904b67
Tom Doherty <tom@singlesecond.com>
2020-10-04 23:56:11 +0100

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # govid
+
+```
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 -d mysql:latest
+brew install mysql-client
+/usr/local/opt/mysql-client/bin/mysql -h127.0.0.1 -uroot -pmy-secret-pw
+create database covid;
+create table covid (DateRep date, Cases int, Deaths int, GeoID varchar(100), Population int);
+curl -sL https://opendata.ecdc.europa.eu/covid19/casedistribution/json >covid.json
+go build
+./govid
+```
--- govid.go
+++ govid.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"time"
 
+	"io"
 	"io/ioutil"
 
 	"encoding/json"
@@ -15,7 +15,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type CovidRecord struct {
+type covidRecord struct {
 	DateRep                                          string `json:"dateRep"`
 	Day                                              string `json:"day"`
 	Month                                            string `json:"month"`
@@ -29,74 +29,69 @@ type CovidRecord struct {
 	ContinentExp                                     string `json:"continentExp"`
 	CumulativeNumberFor14DaysOfCOVID19CasesPer100000 string `json:"Cumulative_number_for_14_days_of_COVID-19_cases_per_100000"`
 }
-type CovidRecords struct {
+
+type covidRecords struct {
 	Records []struct {
-		CovidRecord
+		covidRecord
 	} `json:"records"`
 }
 
+func loadInput(file string, f func(r struct{ covidRecord })) error {
+	infile, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("loadInput(%s): %e", file, err)
+	}
+	defer infile.Close()
+
+	return parseInput(infile, f)
+}
+
+func parseInput(handle io.Reader, f func(r struct{ covidRecord })) error {
+	var records covidRecords
+
+	s, err := ioutil.ReadAll(handle)
+
+	if err != nil {
+		return fmt.Errorf("parseInput(%v): %s", handle, err)
+	}
+
+	if err := json.Unmarshal(s, &records); err != nil {
+		return fmt.Errorf("parseInput(%v) json.Unmarshal: %s", handle, err)
+	}
+
+	for _, record := range records.Records {
+		f(record)
+	}
+	return nil
+}
+
 func main() {
-	f, err := os.Open("covid.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	s, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 -d mysql:latest
 	// brew install mysql-client
 	// /usr/local/opt/mysql-client/bin/mysql -h127.0.0.1 -uroot -pmy-secret-pw
+	// create database covid;
 	// create table covid (DateRep date, Cases int, Deaths int, GeoID varchar(100), Population int);
+
 	db, err := sql.Open("mysql", "root:my-secret-pw@/covid")
+
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
 	defer db.Close()
 
 	stmtIns, err := db.Prepare("INSERT INTO covid VALUES( ?, ?, ?, ?, ? )")
+
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
 	defer stmtIns.Close()
 
-	var records CovidRecords
-	if err := json.Unmarshal(s, &records); err != nil {
-		log.Fatal(err)
-	}
-	totals := map[int]struct{ CovidRecord }{}
-	var highest struct{ CovidRecord }
-
-	for _, record := range records.Records {
-		time, err := time.Parse("02/01/2006", record.DateRep)
+	loadInput("covid.json", func(r struct{ covidRecord }) {
+		time, err := time.Parse("02/01/2006", r.DateRep)
+		_, err = stmtIns.Exec(time, r.Cases, r.Deaths, r.GeoID, r.PopData2019)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if record.Deaths > highest.Deaths {
-			highest = record
-		}
-		totals[record.Deaths] = record
-		//		log.Println(record.DateRep, record.Deaths, time)
-		//DateRep varchar(10), Cases int, Deaths int, GeoID varchar(4), Population int )
-		_, err = stmtIns.Exec(time, record.Cases, record.Deaths, record.GeoID, record.PopData2019)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	keys := make([]int, 0, len(totals))
-
-	for _, i := range totals {
-		keys = append(keys, i.Deaths)
-	}
-	sort.Ints(keys)
-	for _, i := range keys {
-		fmt.Printf("%s\t%s\t%d\t%d\n", totals[i].DateRep, totals[i].GeoID, totals[i].PopData2019, totals[i].Deaths)
-	}
-
-	//	log.Printf("%+v", highest)
-
+	})
 }
